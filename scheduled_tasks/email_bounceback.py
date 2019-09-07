55b453be80619e94305c65141fc76be15266c3f1
sumanthratna <sumanthratna@gmail.com>
2019-09-06 20:40:17 -0400

    Close #28

--- scheduled_tasks/email_bounceback.py
+++ scheduled_tasks/email_bounceback.py
@@ -7,6 +7,7 @@ import re
 import base64
 from keys import GMAIL_SENDER_ADDRESS, GMAIL_SENDER_PASSWORD
 import os
+from typing import List
 
 # Change current working directory, only needed for Atom
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -38,10 +39,10 @@ def bounceback_check() -> None:
 
     try:
         message_list = unread_messages['messages']
-    except:
+    except(Exception):
         return []
 
-    final_list = []  # Final list of undeliverable messages which need to be covered
+    final_list: List = []  # Final list of undeliverable messages which need to be covered
 
     for message in message_list:
         message_id = message['id']
@@ -65,17 +66,21 @@ def bounceback_check() -> None:
         if to_append is True:
             snippet = message['snippet']
 
-            email_which_bounced_regex = re.search(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?", snippet)
+            email_which_bounced_regex = re.search(
+                r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?", snippet)
             email_which_bounced = email_which_bounced_regex.group(
                 0) if email_which_bounced_regex else ""
 
-            message = GMAIL.users().messages().get(userId=user_id, id=message_id,
+            message = GMAIL.users().messages().get(userId=user_id,
+                                                   id=message_id,
                                                    format='raw').execute()
-            message_with_bytes = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+            message_with_bytes = base64.urlsafe_b64decode(
+                message['raw'].encode('ASCII'))
             email_object = email.message_from_bytes(message_with_bytes)
             email_object_as_string = email_object.as_string()
 
-            application_id_object = re.search(r"Applicant-ID: \w{10}", email_object_as_string)
+            application_id_object = re.search(
+                r"Applicant-ID: \w{10}", email_object_as_string)
             application_id_string = application_id_object.group(
                 0) if application_id_object else ""
 
@@ -85,8 +90,8 @@ def bounceback_check() -> None:
                 final_list.append([email_which_bounced, application_id])
 
         # This will mark the message as read
-        GMAIL.users().messages().modify(userId=user_id, id=message_id,
-                                        body={'removeLabelIds': ['UNREAD']}).execute()
+        GMAIL.users().messages().modify(userId=user_id, id=message_id, body={
+            'removeLabelIds': ['UNREAD']}).execute()
 
     return final_list
 
@@ -99,12 +104,16 @@ def bounceback_email(final_list) -> None:
             to='raunakdaga@gmail.com',
             # to='info@elections.virginia.gov',
             subject=f'Absentee Ballot Request (Bounceback) - Applicant-ID: {pair[1]}',
-            contents='This email is being automatically delivered by the eAbsentee absentee ballot application ' + \
-            'system. Our website, eAbsentee.com, is used to request absentee ballots online in an easier manner. ' + \
-            'You can contact us at eAbsentee@gmail.com. Thank you.' + \
-            'Please find attached an absentee ballot request which was unsuccesfully delivered to a registrar due to a bounceback email. ' + \
-            f'The email which caused the bounceback was {pair[0]}. We would apreciate if you could update us on the correct registrar email for this locality.',
-            attachments='../applications/' + str(pair[1]) + '.pdf'
+            contents='This email is being automatically delivered by the ' + \
+            'eAbsentee absentee ballot application system. Our website, ' + \
+            'eAbsentee.com, is used to request absentee ballots online in an ' + \
+            ' easier manner. You can contact us at eAbsentee@gmail.com. ' + \
+            'Thank you. Please find attached an absentee ballot request ' + \
+            'which was unsuccesfully delivered to a registrar due to a ' + \
+            'bounceback email. The email which caused the bounceback was ' + \
+            f'{pair[0]}. We would apreciate if you could update us on the ' + \
+            'correct registrar email for this locality.',
+            attachments=f'../applications/{str(pair[1])}.pdf'
         )
 
 
