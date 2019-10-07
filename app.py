b38b7d4282723cb85149f50d979f4a927ed2de2e
Raunak <raunakdaga@gmail.com>
2019-10-07 11:55:25 -0400

    Cleaned up all routes

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, session
 from flask import send_file, make_response, send_from_directory
-from functions import application_process, add_to_campaign
-from functions import get_ids_and_counties, email_report_api
+from functions import application_process, add_to_campaign, get_ids_and_counties, email_report_api
 from keys import SECRET_KEY, API_KEY
 import os
+import time
 
 # Sets CWD to whatever directory app.py is located in
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -34,40 +34,46 @@ def error_page():
 def credits_page():
     return render_template('credits.html')
 
+# List of Counties for API
+@app.route('/listcounty/')
+def list_of_counties():
+    return(render_template('list_of_counties.html'))
 
+
+# About
+@app.route('/about/')
+def about():
+    return(render_template('about.html'))
+
+
+@app.route('/confirmation/', methods=['GET'])
+def confirmation_page():
+    """Confirmation Route: user is redirected here after submission of form. """
+    return render_template('confirmation.html')
+
+
+'''Static PDF Routes'''
 # Fillable Form as .PDF
 @app.route('/fillableform/')
 def render_fillableform_pdf():
     return send_file(
-        open('static/pdf/blankAppFillable.pdf', 'rb'),
-        attachment_filename='blankAppFillable.pdf'
+        open('static/pdf/blankAppFillable.pdf', 'rb'), attachment_filename='blank_app_fillable.pdf'
     )
 
 # Printable Form as .PDF
 @app.route('/printform/')
 def render_printform_pdf():
     return send_file(
-        open('static/pdf/blankApp.pdf', 'rb'),
-        attachment_filename='blankApp.pdf'
+        open('static/pdf/blankApp.pdf', 'rb'), attachment_filename='blank_app_printable.pdf'
     )
 
 
 @app.route('/videocredits/')
 def render_videocredits_pdf():
     return send_file(
-        open('static/pdf/credits.pdf', 'rb'),
-        attachment_filename='credits.pdf'
+        open('static/pdf/credits.pdf', 'rb'), attachment_filename='credits.pdf'
     )
 
-# List of Counties for API
-@app.route('/listcounty/')
-def list_of_counties():
-    return(render_template('list_of_counties.html'))
-
-# About
-@app.route('/about/')
-def about():
-    return(render_template('about.html'))
 
 # 404
 @app.errorhandler(404)
@@ -83,54 +89,39 @@ def process_form():
     built into the PDF, and emailed to the respective registar. If
     unable to send the PDF, an error page is returned. """
     if request.method == 'POST':
-        # try:
-        application_process(request)
-        # except(Exception):
-        #     return redirect('/error/')
+        try:
+            application_process(request)
+        except(Exception):
+            return redirect('/error/')
         return redirect('/confirmation/')
     else:
+        ids_and_counties = ''
         if 'campaign' in request.cookies:
-            ids_and_counties = get_ids_and_counties(request)
-
-            return render_template('form_counties.html',
-                                   ids_and_counties=ids_and_counties)
-        return render_template('form.html')
+            ids_and_counties = get_ids_and_counties(request.cookies.get('campaign'))
+        else:
+            ids_and_counties = get_ids_and_counties('allcounties')
+        return render_template('form_counties.html', ids_and_counties=ids_and_counties)
 
 
 @app.route('/formdev/', methods=['POST', 'GET'])
 def form_dev():
-    """ Form Route: Returns form when requested,
-    collects data from user when inputted. The data is sent to
-    functions.py, where it is parsed and converted,
-    built into the PDF, and emailed to the respective registar. If
-    unable to send the PDF, an error page is returned. """
     if request.method == 'POST':
-        # try:
         application_process(request)
-        # except(Exception):
-        #     return redirect('/error/')
         return redirect('/confirmation/')
     else:
-        return render_template('formtest.html')
+        ids_and_counties = get_ids_and_counties('dagaprez')
+        return render_template('form_counties.html', ids_and_counties)
 
 
 @app.route('/form/<group>/', methods=['POST', 'GET'])
-def form_surovell(group: str):
-    """ Form Route: Returns form when requested,
-    collects data from user when inputted. The data is sent to
-    functions.py, where it is parsed and converted,
-    built into the PDF, and emailed to the respective registar. If
-    unable to send the PDF, an error page is returned. """
+def form_group(group: str):
     if request.method == 'POST':
-        # try:
-        application_process(request, group)
-        # except(Exception):
-        #     return redirect('/error/')
+        try:
+            application_process(request, group)
+        except(Exception):
+            return redirect('/error/')
         return redirect('/confirmation/')
     else:
-        # response = make_response(render_template('form.html'))
-        # response.set_cookie('group', group, max_age=60 * 60 * 24 * 365 * 2)
-        # return response
         return render_template('form.html')
 
 
@@ -143,33 +134,19 @@ def home_with_campaign(campaign: str):
     return response
 
 
-# Sets group cookies - determines which group the users came from
 @app.route('/group/<group>')
-def set_channel(group: str):
+def set_group(group: str):
+    """This route sets the group cookies."""
     response = make_response(redirect('/'))
     response.set_cookie('group', group, max_age=60 * 60 * 24 * 365 * 2)
     return response
 
 
-# TODO: Redirect to page based off of succesful submission
-@app.route('/confirmation/', methods=['GET'])
-def confirmation_page():
-    """Confirmation Route: user is redirected here
-    after submission of form. """
-    # return render_template('confirmation.html')
-    # if session.get('output_pdf') is not None:
-    return render_template('confirmation.html')
-    # else:
-    #     # TODO: redirect to more appropriate error page (like 403 forbidden)
-    #     return redirect('/404/')
-
-
-# Displays application to user in PDF form
 @app.route('/applications/<id>.pdf')
 def render_pdf(id: str):
+    '''Displays application to user in PDF form'''
     return send_file(
-        open(session['output_file'], 'rb'),
-        attachment_filename=session['output_file']
+        open(session['output_file'], 'rb'), attachment_filename=session['output_file']
     )
 
 
@@ -186,6 +163,9 @@ def api():
         return render_template('api.html')
     else:
         return render_template('api.html')
+
+
+''' FAVICONS '''
 
 
 @app.route('/apple-touch-icon.png', methods=['GET'])
