a3d60e05885b012de08ab51fa3cb006e8f58fa41
Raunak Daga <raunakdaga@gmail.com>
2020-04-26 15:54:09 -0500

    Fix formatting

--- scheduled_tasks/email_report.py
+++ scheduled_tasks/email_report.py
@@ -1,27 +1,22 @@
 import yagmail
 from datetime import date
-from keys import GMAIL_SENDER_ADDRESS, GMAIL_SENDER_PASSWORD
 import os
 import openpyxl
-from openpyxl import load_workbook
 import json
 import sys
+from keys import GMAIL_SENDER_ADDRESS, GMAIL_SENDER_PASSWORD
+from openpyxl import load_workbook
 
-# Change current working directory, only needed for Atom
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
-def email_report_daily() -> None:
-    # print(os.path.dirname(os.path.realpath(__file__)))
-    """Email the Excel spreadsheet to Senator Surovell and Mr. Rouvelas. """
-    today_date: str = date.today().strftime("%m-%d-%y")
+def email_report_daily():
+    today_date = date.today().strftime("%m-%d-%y")
     report_path = f'../reports/dailyreports/{today_date}.xlsx'
-    report: openpyxl.workbook.Workbook = load_workbook(filename=report_path)
-    worksheet: openpyxl.worksheet.worksheet.Worksheet = report.active
+    report = load_workbook(filename=report_path)
+    worksheet = report.active
     if worksheet['A2'].value:
         yagmail.SMTP(GMAIL_SENDER_ADDRESS, GMAIL_SENDER_PASSWORD).send(
             to=['raunak@eabsentee.org', 'larry@eabsentee.org', 'robert@eabsentee.org'],
-            # to='raunakdaga@gmail.com',
             subject=f'Daily Absentee Ballot Application Report - {today_date}',
             contents=f'Please find attached the daily report of absentee ' +
             f'ballot applications for {today_date}.',
@@ -30,13 +25,11 @@ def email_report_daily() -> None:
 
 
 def email_report(file_name, emails):
-    today_date: str = date.today().strftime("%m-%d-%y")
-    report: openpyxl.workbook.Workbook = load_workbook(filename=file_name)
-    worksheet: openpyxl.worksheet.worksheet.Worksheet = report.active
+    today_date = date.today().strftime("%m-%d-%y")
+    report = load_workbook(filename=file_name)
+    worksheet = report.active
     if worksheet['B' + str(worksheet.max_row)].value.split()[0] == today_date:
-        # print("I am now emailing the group: " + file_name)
         yagmail.SMTP(GMAIL_SENDER_ADDRESS, GMAIL_SENDER_PASSWORD).send(
-            # to='raunakdaga@gmail.com',
             to=emails,
             cc=['raunak@eabsentee.org', 'larry@eabsentee.org'],
             subject=f'Daily Report - eAbsentee Applications',
@@ -48,12 +41,10 @@ def email_report(file_name, emails):
 
 
 def email_all_groups():
-    print(os.getcwd())
     with open('../static/groups.json') as file:
         groups_json = json.load(file)
         groups = groups_json.keys()
         for group in groups:
-            # print("Group attempting: " + group)
             if os.path.isfile('../reports/' + group + '.xlsx'):
                 try:
                     email_report('../reports/' + group + '.xlsx',
