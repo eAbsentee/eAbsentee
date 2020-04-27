cfc7105377d413f88f405bb6897845389466fe01
Raunak Daga <raunakdaga@gmail.com>
2020-04-27 15:10:44 -0500

    Added back error catching

--- app.py
+++ app.py
@@ -21,84 +21,59 @@ app.config['DEBUG'] = True
 
 
 '''Static Routes'''
-# Homepage
+
+"""Home Route"""
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
+"""Form Error Route: users are directed here if an error is encountered during the processing of their application"""
 @app.route('/error/')
 def error_page():
     return render_template('formerror.html')
 
-
+"""Credits Route: credits for the template, developers, and team behind the website"""
 @app.route('/credits/', methods=['GET'])
 def credits_page():
     return render_template('credits.html')
 
-# List of Counties w/ Matching number ID's for API information
+"""List of Counties Route: displays all counties and their matching numerical ID as information for the API"""
 @app.route('/listcounty/')
 def list_of_counties():
     return(render_template('list_of_counties.html'))
 
-
+"""About Route: displays information about eAbsentee and Vote Absentee Virginia."""
 @app.route('/about/')
 def about():
     return(render_template('about.html'))
 
 
+"""Confirmation Route: user is redirected here after submission of form. """
 @app.route('/confirmation/')
 def confirmation_page():
-    """Confirmation Route: user is redirected here after submission of form. """
     return render_template('confirmation.html')
 
-
+"""Form Closed Route: user is redirected here from the form route when absentee ballot applications are no longer being accepted."""
 @app.route('/formclosed/')
 def form_closed():
     return render_template("formclosed.html")
 
-# Bills
-#
-# @app.route('/bills/hb1/')
-# def hb1():
-#     return render_template("hb1.html")
-#
-#
-# @app.route('/bills/hb201/')
-# def hb201():
-#     return render_template("hb201.html")
-#
-#
-# @app.route('/bills/hb207/')
-# def hb207():
-#     return render_template("hb207.html")
-#
-#
-# @app.route('/bills/hb220/')
-# def hb220():
-#     return render_template("hb220.html")
-#
-#
-# @app.route('/bills/hb238/')
-# def hb238():
-#     return render_template("hb238.html")
 
+"""Static PDF Routes"""
 
-'''Static PDF Routes'''
-# Fillable Form as .PDF
+"""Fillable Form"""
 @app.route('/fillableform/')
 def render_fillableform_pdf():
     return send_file(
         open('static/pdf/blank_app_fillable.pdf', 'rb'), attachment_filename='blank_app_fillable.pdf'
     )
 
-# Printable Form as .PDF
+"""Printable Form"""
 @app.route('/printform/')
 def render_printform_pdf():
     return send_file(
         open('static/pdf/blank_app_printable.pdf', 'rb'), attachment_filename='blank_app_printable.pdf'
     )
-
 
 @app.route('/videocredits/')
 def render_videocredits_pdf():
@@ -107,9 +82,9 @@ def render_videocredits_pdf():
     )
 
 
+"""Displays application using session variables set in functions.py"""
 @app.route('/applications/<id>.pdf')
 def render_pdf(id):
-    '''Displays application to user in PDF form'''
     return send_file(
         open(session['output_file'], 'rb'), attachment_filename=session['output_file']
     )
@@ -131,10 +106,10 @@ def form():
     """ Form Route: Returns form when requested, collects data from user when inputted. The data is sent to functions.py, where it is parsed and converted, built into the PDF, and emailed to the respective registar. If
     unable to send the PDF, an error page is returned. """
     if request.method == 'POST':
-        # try:
-        application_process(request)
-        # except(Exception):
-        #     return redirect('/error/')
+        try:
+            application_process(request)
+        except(Exception):
+            return redirect('/error/')
         return redirect('/confirmation/')
     else:
         if 'group' in request.cookies:
@@ -148,11 +123,10 @@ def form():
 @app.route('/form/<group>/', methods=['POST', 'GET'])
 def form_group(group):
     if request.method == 'POST':
-        # try:
-        application_process(request, group)
-        # except(Exception):
-        #     print(Exception)
-        # return redirect('/error/')
+        try:
+            application_process(request, group)
+        except(Exception):
+            return redirect('/error/')
         return redirect('/confirmation/')
     else:
         return render_template('form.html', ids_and_counties=get_ids_and_counties(group))
