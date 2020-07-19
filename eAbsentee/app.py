29f5f4567c4601e8feb500bfa610375ce30d9761
Raunak Daga <raunakdaga@gmail.com>
2020-07-19 02:30:33 -0500

    Db fixes

--- eAbsentee/app.py
+++ eAbsentee/app.py
@@ -7,18 +7,18 @@ from eAbsentee.config import Config
 
 
 load_dotenv()
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # db.init_app(app)
+    db.init_app(app)
     sys.path.append(os.path.join(os.path.dirname(__file__)))
     with app.app_context():
         from eAbsentee.form.models import User
 
-        # db.create_all()
+        db.create_all()
 
         from eAbsentee.form import form
         from eAbsentee.home import home
