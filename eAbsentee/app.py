5f1ed466dcee228ea3f8c2180cecffc49f84c21f
Raunak Daga <raunakdaga@gmail.com>
2020-07-19 21:18:33 -0500

    Updated app

--- eAbsentee/app.py
+++ eAbsentee/app.py
@@ -2,36 +2,46 @@ import os, sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 load_dotenv()
 
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
+    set_config(app)
+    init_apps(app)
+    with app.app_context():
+        database_models(db)
+        register_blueprints(app)
+        return app
 
+def set_config(app):
     from eAbsentee.config import Config
     app.config.from_object(Config)
 
+def init_apps(app):
     db.init_app(app)
     login_manager.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.login_view = 'login'
 
-    sys.path.append(os.path.join(os.path.dirname(__file__)))
-    
-    with app.app_context():
-        from eAbsentee.form.models import User
-        from eAbsentee.admin.models import AdminUser
-        if os.environ['TESTING_MODE']:
-            db.create_all()
+def database_models(db):
+    from eAbsentee.form.models import User
+    from eAbsentee.admin.models import AdminUser
+    if os.environ['TESTING_MODE']:
+        db.create_all()
 
-        from eAbsentee.form import form
-        from eAbsentee.home import home
-        from eAbsentee.admin import admin
+def register_blueprints(app):
+    from eAbsentee.form import form
+    from eAbsentee.home import home
+    from eAbsentee.admin import admin
 
-        app.register_blueprint(form.form_bp)
-        app.register_blueprint(home.home_bp)
-        app.register_blueprint(admin.admin_bp)
-
-        return app
+    app.register_blueprint(form.form_bp)
+    app.register_blueprint(home.home_bp)
+    app.register_blueprint(admin.admin_bp)
