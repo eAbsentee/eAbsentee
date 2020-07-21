1e876d2336f44758c6818048d7e5cc25831e6b91
Raunak Daga <raunakdaga@gmail.com>
2020-07-21 01:24:58 -0500

    Form fixed

--- eAbsentee/form/form.py
+++ eAbsentee/form/form.py
@@ -1,6 +1,8 @@
+import os
 from flask import Blueprint
 from flask import render_template, request, redirect
 from .utils import application_process, add_to_database_all_voters
+from dotenv import load_dotenv
 
 form_bp = Blueprint(
     'form_bp',
@@ -22,17 +24,20 @@ def form_closed():
     return render_template('formclosed.html')
 
 @form_bp.route('/db/')
-def poop():
+def add_to_database():
     add_to_database_all_voters()
     return render_template('formclosed.html')
 
 @form_bp.route('/form/', methods=['POST', 'GET'])
 def form():
     if request.method == 'POST':
-        # try:
-        application_process(request)
-        # except(Exception):
-            # return redirect('/error/')
+        if os.environ["FLASK_DEBUG"]:
+            application_process(request)
+        else:
+            try:
+                application_process(request)
+            except(Exception):
+                return redirect('/error/')
         return redirect('/confirmation/')
     else:
         return render_template('form.html')
@@ -40,10 +45,13 @@ def form():
 @form_bp.route('/form/<group>/', methods=['POST', 'GET'])
 def form_group(group):
     if request.method == 'POST':
-        try:
-            application_process(request, group)
-        except(Exception):
-            return redirect('/error/')
+        if os.environ["FLASK_DEBUG"]:
+            application_process(request)
+        else:
+            try:
+                application_process(request, group)
+            except(Exception):
+                return redirect('/error/')
         return redirect('/confirmation/')
     else:
         return render_template('form.html')
