8c46a0599230e9665014b9a51b37f4ca792371eb
Raunak Daga <raunakdaga@gmail.com>
2020-07-21 01:27:20 -0500

    Lol tired

--- eAbsentee/form/utils.py
+++ eAbsentee/form/utils.py
@@ -24,7 +24,7 @@ def application_process(request, group_code_form=None):
     data = parse_data(request, group_code_form=group_code_form)
     write_pdf(data)
     add_to_database(data)
-    if not os.environ["DEBUG_MODE"]:
+    if not os.environ["FLASK_DEBUG"]:
         email_registrar(data)
     os.remove(data['application_id'] + '.pdf')
 
