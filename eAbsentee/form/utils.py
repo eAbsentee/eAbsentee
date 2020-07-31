9072764b91873752ec3548c417ff11dad1f4a423
Raunak <raunakdaga@gmail.com>
2020-07-31 13:00:20 -0400

    Proper utils

--- eAbsentee/form/utils.py
+++ eAbsentee/form/utils.py
@@ -1,19 +1,16 @@
-import hashlib
-import yagmail
 import os, sys
+import yagmail
 import json
-import datetime
 import io
-import csv
+import random
+import string
 from flask import request
 from datetime import date
-from datetime import datetime
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from dotenv import load_dotenv
 from .models import db, User
-from sqlalchemy.exc import IntegrityError
 
 # Change current working directory to directory 'functions.py' is in.
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -21,138 +18,73 @@ load_dotenv()
 
 
 def application_process(request, group_code=None):
-    data = parse_data(request, group_code=group_code)
-    write_pdf(data)
-    add_to_database(data)
-    if not os.environ["FLASK_DEBUG"]:
-        email_registrar(data)
-    os.remove(data['output_file'])
+    application_id = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(24))
+    write_pdf(application_id, request)
+    add_to_database(application_id, request, group_code=group_code)
+    email_registrar(application_id, request)
+    os.remove(application_id + '.pdf')
 
-def parse_data(request, group_code):
-    data = {}
+def write_pdf(application_id, request):
     today_date = date.today().strftime('%m%d%y')
 
-    if group_code is not None:
-        group_code = group_code.lower()
-    elif request.cookies.get('group'):
-        group_code = request.cookies.get('group').lower()
-    elif group_code is None:
-        group_code = ''
-
-
-    emails_to_be_sent_to = []
-    with open('../static/localities_info.json') as file:
-        localities = json.load(file)
-        if os.environ["FLASK_DEBUG"] == True:
-            emails_to_be_sent_to = ['applications@eabsentee.org']
-        else:
-            emails_to_be_sent_to = [localities[request.form['registered_county']]['email']]
-
-        if request.form.get('email'):
-            emails_to_be_sent_to.append(request.form.get('email'))
-
-    phonenumber = request.form.get('phonenumber').replace(
-        '-', '').replace('(', '').replace(')', '').replace(' ', '').replace('+1', '').replace('-', '').replace('.', '').replace('+', '')
-
-    with open('../static/localities_info.json') as file:
-        localities = json.load(file)
-        data = {
-            'first_name': request.form['name_first'],
-            'middle_name': request.form['name_middle'],
-            'last_name': request.form['name_last'],
-            'suffix': request.form['name_suffix'],
-            'full_name': request.form['name_first'] + ' ' + request.form['name_middle'] + ' ' + request.form['name_last'],
-            'ssn': '  '.join(request.form['ssn']),
-            'registered_to_vote': request.form['registered_county'],
-            'email': request.form['email'],
-            'address': request.form['address'],
-            'apt': request.form['apt'],
-            'city': request.form['city'],
-            'zip_code': '   '.join(request.form['zip']),
-            'state': 'VA',
-            'full_address': request.form['address'] + ((' ' + request.form['apt']) if request.form['apt'] else '') + ', ' + request.form['city'] + ', ' + 'VA' +  ' ' + request.form['zip'],
-            'full_delivery_address': request.form['different_address'] +      ((' ' + request.form['different_apt']) if request.form['different_apt'] else ' ') + ', ' + request.form['different_city'] + ', ' + request.form['different_state'] +  ' ' + request.form['different_zip'] + ' ' + request.form['different_country'],
-            'delivery_address': request.form.get('different_address'),
-            'delivery_city': request.form.get('different_city'),
-            'delivery_apt': request.form.get('different_apt'),
-            'delivery_zip': '   '.join(request.form.get( 'different_zip').replace('-', '')),
-            'delivery_country': request.form['different_country'] if request.form['different_city'] else '',
-            'delivery_state': request.form.get('different_state'),
-            'signature': '/S/ ' + request.form[
-                'signature'].replace('/S/', '', 1).strip(),
-            'phonenumber': phonenumber,
-            'assistant_check': 'X' if request.form.get(
-                'assistance_check') == 'true' else '',
-            'assistant_fullname': request.form.get('assistant_name'),
-            'assistant_address': request.form.get('assistant_address'),
-            'assistant_signature': request.form.get('assistant_name'),
-            'assistant_apt': request.form.get('assistant_apt'),
-            'assistant_city': request.form.get('assistant_city'),
-            'assistant_state': request.form.get('assistant_state'),
-            'assistant_zip': '   '.join(request.form.get(
-                'assistant_zip').replace('-', '')),
-            'deliver_residence': 'X' if request.form.get(
-                'different_address_check') == 'false' else '',
-            'deliver_mailing': 'X' if request.form.get(
-                'different_address_check') == 'true' else '',
-            'county_check': 'X' if 'County' in localities[
-                request.form['registered_county']]['locality'] else '',
-            'city_check': 'X' if 'City' in localities[
-                request.form['registered_county']]['locality'] else '',
-            'date_today_month': today_date[0:2],
-            'date_today_day': today_date[2:4],
-            'date_today_year': today_date[4:6],
-            'application_ip': request.environ.get('HTTP_X_REAL_IP', request.remote_addr),
-            'group_code': group_code,
-            'emails_to_be_sent_to': emails_to_be_sent_to,
-            'lat': request.form.get('lat'),
-            'long': request.form.get('long')
-        }
-
-    data['application_id'] = hashlib.md5(repr(data).encode('utf-8')).hexdigest()[: 24]
-    data['output_file'] = data['application_id'] + '.pdf'
-
-    return data
-
-def write_pdf(data):
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=letter)
-    can.drawString(180, 732, data['last_name'])
-    can.drawString(410, 732, data['first_name'])
-    can.drawString(190, 715, data['middle_name'])
-    can.drawString(405, 715, data['suffix'])
+    can.drawString(180, 732, request.form['name_last'])
+    can.drawString(410, 732, request.form['name_first'])
+    can.drawString(190, 715, request.form['name_middle'])
+    can.drawString(405, 715, request.form['name_suffix'])
 
-    can.drawString(525, 698, (data['ssn'])) # SSN
+    can.drawString(525, 698, '  '.join(request.form['ssn'])) # SSN
     can.drawString(282, 683, 'X') # Gen/Spec
     # can.drawString(405, 683, 'X') # Dem Prim
     # can.drawString(503, 683, 'X') # Republican Primary
     can.drawString(212, 670, '11') # Month of Election
     can.drawString(261, 670, '03') # Day of Election
     can.drawString(310, 670, '20') # Year of Election
-    can.drawString(428, 670, data['registered_to_vote']) # City/County of
+    can.drawString(428, 670, request.form['registered_county']) # City/County
     # can.drawString(409, 658, 'X') # Vote by Mail in All Elections Yes
     # can.drawString(442, 658, 'X') # Vote by Mail in All Elections No
     # can.drawString(126, 633, 'X') # Dem Primary Ballots
     # can.drawString(234, 633, 'X') # Rep Primary Ballots
     # can.drawString(331, 633, 'X') # No Primary Ballots
 
-    can.drawString(165, 614, data['address'])
-    can.drawString(535, 614, data['apt'])
-    can.drawString(145, 596, data['city'])
-    can.drawString(408, 596, ''.join(data['zip_code']))
+    can.drawString(165, 614, request.form['address'])
+    can.drawString(535, 614, request.form['apt'])
+    can.drawString(145, 596, request.form['city'])
+    can.drawString(408, 596, '   '.join(request.form['zip']))
 
-    can.drawString(165, 565, data['delivery_address'])
-    can.drawString(545, 565, data['delivery_apt'])
-    can.drawString(145, 548, data['delivery_city'])
-    can.drawString(322, 548, data['delivery_state'])
-    can.drawString(418, 548, ''.join(data['delivery_zip']))
-    can.drawString(558, 548, data['delivery_country'])
+    can.drawString(198, 480, request.form['former_name'])
+    can.drawString(198, 464, request.form['former_address'])
+    # can.drawString(158, 448, 'City')
+    # can.drawString(398, 448, 'State')
+    # can.drawString(477, 448, '     '.join('20171'))
+    can.drawString(525, 464, request.form['date_moved'])
 
-    can.drawString(175, 509, data['email'])
-    can.drawString(275, 115, data['signature'])
-    can.drawString(485, 115, data['date_today_month'])
-    can.drawString(525, 115, data['date_today_day'])
-    can.drawString(565, 115, data['date_today_year'])
+
+    can.drawString(128, 301, 'X' if request.form.get(
+        'assistance_check') == 'true' else '',)
+    can.drawString(210, 258, request.form['assistant_name'])
+    # can.drawString(510, 258, 'Asssitant Phone')
+    can.drawString(210, 240, request.form['assistant_address'])
+    can.drawString(560, 240, request.form['assistant_apt'])
+    can.drawString(150, 226, request.form['assistant_city'])
+    can.drawString(385, 226, request.form['assistant_state'])
+    can.drawString(482, 226, '     '.join(request.form['assistant_zip']))
+    can.drawString(210, 176, request.form['assistant_name'])
+    can.drawString(460, 176, today_date)
+
+    can.drawString(165, 565, request.form['different_address'])
+    can.drawString(545, 565, request.form['different_apt'])
+    can.drawString(145, 548, request.form['different_city'])
+    can.drawString(322, 548, request.form['different_state'])
+    can.drawString(418, 548, '   '.join(request.form['different_zip']))
+    can.drawString(558, 548, request.form['different_country'] if request.form['different_city'] else '')
+
+    can.drawString(175, 509, request.form['email'])
+    can.drawString(275, 115, '/s/ ' + request.form['signature'].strip())
+    can.drawString(485, 115, today_date[0:2])
+    can.drawString(525, 115, today_date[2:4])
+    can.drawString(565, 115, today_date[4:6])
 
     can.save()
     packet.seek(0)
@@ -163,36 +95,47 @@ def write_pdf(data):
     page.mergePage(new_pdf.getPage(0))
     output.addPage(page)
 
-    with open(data['output_file'], 'wb') as output_pdf_file:
+    with open(application_id + '.pdf', 'wb') as output_pdf_file:
         output.write(output_pdf_file)
 
-def add_to_database(data):
-    new_voter = User(
-        application_id=data['application_id'],
-        name=data['full_name'],
-        county=data['registered_to_vote'],
-        email=data['email'],
-        phonenumber=data['phonenumber'],
-        full_address=data['full_address'],
-        ip=data['application_ip'],
-        group_code=data['group_code'],
-        lat=data['lat'],
-        long=data['long']
-    )
+def add_to_database(application_id, request, group_code):
+    if group_code is not None:
+        group_code = group_code.lower()
+    elif request.cookies.get('group'):
+        group_code = request.cookies.get('group').lower()
+    elif group_code is None:
+        group_code = ''
 
-    print(new_voter)
+    new_voter = User(
+        application_id=application_id,
+        name=(request.form['name_first'] + ' ' + request.form['name_middle'] + ' ' + request.form['name_last']).replace('  ', ' ').strip(),
+        county=request.form['registered_county'],
+        email=request.form['email'],
+        phonenumber=request.form['phonenumber'],
+        full_address=(request.form['address'] + ((' ' + request.form['apt']) if request.form['apt'] else '') + ', ' + request.form['city'] + ', ' + 'VA' +  ' ' + request.form['zip']),
+        ip=request.environ.get('HTTP_X_REAL_IP', request.remote_addr),
+        group_code=group_code,
+        lat=request.form['lat'],
+        long=request.form['long']
+    )
 
     db.session.add(new_voter)
     db.session.commit()
 
 
-def email_registrar(data):
-    """Email the form to the registrar of the applicant's locality. """
+def email_registrar(application_id, request):
+    emails_to_send = []
+    with open('../static/localities_info.json') as file:
+        localities = json.load(file)
+        if not os.environ["FLASK_DEBUG"]:
+            emails_to_send.append(localities[request.form['registered_county']]['email'])
+        if request.form.get('email'):
+            emails_to_send.append(request.form.get('email'))
+
     yagmail.SMTP(os.environ["GMAIL_SENDER_ADDRESS"], os.environ["GMAIL_SENDER_PASSWORD"]).send(
-        to=([email for email in data['emails_to_be_sent_to']]),
+        to=([email for email in emails_to_send]),
         subject=('Absentee Ballot Request - Applicant-ID: ' +
-        f'{data["application_id"]}'),
-        contents='Please find attached an absentee ballot request ' +
-        f'submitted on behalf of {data["full_name"]} from eAbsentee.org',
-        attachments=data['output_file']
+        f'{application_id}'),
+        contents='Please find attached an absentee ballot request submitted from eAbsentee.org',
+        attachments=(application_id + '.pdf')
     )
