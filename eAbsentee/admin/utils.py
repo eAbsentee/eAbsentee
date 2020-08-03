c676cb8e16ba6c68de4a7c1e1c11897671bddd1e
Raunak <raunakdaga@gmail.com>
2020-08-03 11:02:27 -0400

    HTTPS image serving

--- eAbsentee/admin/utils.py
+++ eAbsentee/admin/utils.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# 40.3, -75, 6 creates a default zoom over the entire state of Virginia
 def get_users(group, date):
     markers = []
     for user in User.query.filter_by(group_code=group).all():
         markers.append({
-            'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+            'icon': 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
             'lat': user.lat,
             'lng': user.long,
             'name': user.name
