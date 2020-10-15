01f38febf9ca97bbe48895de57e3bf0caa42671d
Raunak <raunakdaga@gmail.com>
2020-10-15 17:14:54 -0400

    Email groups script created

---
+++ scripts/email_groups.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+# TODO:
+# 1) More secure storage of API key
+# 2) Post URL's shouldn't be public
+
+params = {'API_KEY': os.environ['API_KEY']}
+if os.environ['FLASK_DEBUG'] == 'True':
+    requests.post('http://localhost:5000/api/remind/', params=data)
+else:
+    requests.post('https://www.eabsentee.org/api/remind/', params=data)
