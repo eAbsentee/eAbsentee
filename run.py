4927cdd3a16b922e5e5906acb428ad48625a34f9
Raunak <raunakdaga@gmail.com>
2021-01-17 22:09:15 -0500

    Basic documentation of run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+# Imports the create_app method from the eAbsentee package
+# Flask recognizes the app exists through scanning for the create_app method
+# Run this file using 'flask run' from the CMD line in the root directory 
+
 from eAbsentee.app import create_app
 
 app = create_app()
