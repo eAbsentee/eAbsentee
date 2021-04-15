cc063f5b066efaf97cfdd248715caabd256aad9a
Sumanth Ratna <sumanthratna@gmail.com>
2021-04-15 14:57:07 -0400

    Add create_admin script

---
+++ scripts/create_admin.py
@@ -0,0 +1,26 @@
+from sys import path
+path.append("../eAbsentee")
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+from eAbsentee.admin.models import AdminUser, ACCESS
+from flask_bcrypt import Bcrypt
+from getpass import getpass
+
+engine = create_engine(getenv("SQLALCHEMY_DATABASE_URI"))
+Session = sessionmaker(engine)
+
+with Session() as session:
+    email = input("Enter the new admin's email: ")
+    raw_password = getpass("Enter the new admin's password: ")
+    new_admin = AdminUser(
+        email=email,
+        password=Bcrypt().generate_password_hash(raw_password).decode('utf-8'),
+        access=ACCESS["admin"],
+    )
+    session.add(new_admin)
+    session.commit()
