334a780a5b75b3948f4258877829549b499292d0
Sumanth Ratna <sumanth@eabsentee.org>
2021-06-12 19:20:56 -0400

    Add Spanish template to update_form_template script

--- scripts/update_form_template.py
+++ scripts/update_form_template.py
@@ -2,3 +2,4 @@ from urllib.request import urlretrieve
 
 
 urlretrieve("https://www.elections.virginia.gov/media/castyourballot/SBE-701-703.1.pdf", "eAbsentee/static/pdf/form.pdf")
+urlretrieve("https://www.elections.virginia.gov/media/formswarehouse/absentee-voting/applications/SBE-701-703.1_Spanish_rev7-20.pdf", "eAbsentee/static/pdf/spanish_form.pdf")
