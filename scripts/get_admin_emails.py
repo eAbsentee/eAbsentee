38b45b5539f0b238aceb6a1c22aa2fb80827436b
Sumanth Ratna <sumanth@eabsentee.org>
2021-07-16 18:01:06 -0400

    Read users from group_references table in get_admin_emails script

--- scripts/get_admin_emails.py
+++ scripts/get_admin_emails.py
@@ -20,7 +20,7 @@ with SSHClient() as ssh:
     assert "".join(whoami_stdout.readlines()).strip() == username
 
     # TODO: use SSH forwarding in order to be able to use a Python wrapper for mysql
-    count_stdin, count_stdout, count_stderr = ssh.exec_command(f"mysql -u {username} -h {username}.mysql.pythonanywhere-services.com -e 'SELECT DISTINCT email FROM admin_user' --skip-column-names '{username}$users'")
+    count_stdin, count_stdout, count_stderr = ssh.exec_command(f"mysql -u {username} -h {username}.mysql.pythonanywhere-services.com -e 'SELECT email FROM admin_user UNION SELECT email FROM group_references' --skip-column-names '{username}$users'")
     print("".join(count_stdout.readlines()))
     print("".join(count_stderr.readlines()), file=stderr)
     count_stdin.close()
