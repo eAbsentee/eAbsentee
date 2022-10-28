8a5a3ae8f576e8c3f3f9b01f28b9b07b5dfad428
ShakyW <james.joshua.greene@gmail.com>
2022-10-28 17:02:23 -0400

    Closing form from 2022-10-28 election

--- eAbsentee/config.py
+++ eAbsentee/config.py
@@ -44,7 +44,7 @@ class Config:
 
     # https://www.elections.virginia.gov/casting-a-ballot/calendars-schedules/upcoming-elections.html
     UPCOMING_ELECTIONS = [
-        date(2022, 11, 8),  # General Election
+        #date(2022, 11, 8),  # General Election
     ]
     FORM_OPEN = len(UPCOMING_ELECTIONS) > 0
 
