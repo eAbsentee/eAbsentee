2268872fa4b4cb0863fbd7fbc6e62c45b7460e70
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2023-05-27 19:46:57 -0400

    Update email_tests.py

--- scripts/email_tests.py
+++ scripts/email_tests.py
@@ -87,18 +87,16 @@ with sshtunnel.SSHTunnelForwarder(
     session.add(new_user)
     session.commit()
             
-            
-            
-# temporary storage
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # temporary storage
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-# TODO:
-# 1) More secure storage of API key
-# 2) Post URL's shouldn't be public
+    # TODO:
+    # 1) More secure storage of API key
+    # 2) Post URL's shouldn't be public
 
-params = {'API_KEY': os.environ['API_KEY']}
-if os.environ['FLASK_DEBUG'] == 'True':
-    requests.post('http://localhost:5000/api/testing/', params=params)
-else:
-    requests.post('https://www.eabsentee.org/api/testing/', params=params)
+    params = {'API_KEY': os.environ['API_KEY']}
+    if os.environ['FLASK_DEBUG'] == 'True':
+        requests.post('http://localhost:5000/api/testing/', params=params)
+    else:
+        requests.post('https://www.eabsentee.org/api/testing/', params=params)
