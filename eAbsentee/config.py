1d0b13eda3b604f844012d274bc099ffba3ffb1e
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2023-06-09 17:31:15 -0400

    Update config.py

--- eAbsentee/config.py
+++ eAbsentee/config.py
@@ -44,7 +44,7 @@ class Config:
 
     # https://www.elections.virginia.gov/casting-a-ballot/calendars-schedules/upcoming-elections.html
     UPCOMING_ELECTIONS = [
-        date(2023, 11, 7), # Primary election
+        date(2023, 11, 7), # General election
     ]
     FORM_OPEN = len(UPCOMING_ELECTIONS) > 0
 
