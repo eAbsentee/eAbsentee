9c3b77e7b3bbd15f6d43d20854419b96a785b9b3
Brian <bzhou2024@gmail.com>
2023-07-18 22:36:29 -0400

    Update to 2022 form

--- eAbsentee/form/utils.py
+++ eAbsentee/form/utils.py
@@ -41,55 +41,59 @@ def write_pdf(application_id, request, lang):
     can.drawString(405, 732, request.form['name_first'].title())
     can.drawString(172, 716, request.form['name_middle'].title())
     can.drawString(405, 716, request.form['name_suffix'])
-    can.drawString(513, 699, '   '.join(request.form['ssn']))  # SSN
+    can.drawString(353, 700, '   '.join(request.form['ssn']))  # SSN
+    can.drawString(504, 700, '  '.join(request.form['birthyear']))  # DOB
 
     if 'permanent_absentee' in request.form and request.form['permanent_absentee'] == 'on':
-        can.drawString(341, 682, 'X') # Vote by Mail in All Elections Yes
+        can.drawString(340, 600, 'X') # Vote by Mail in All Elections Yes
 
         if 'permanent_absentee_primary_party' in request.form:
             if request.form['permanent_absentee_primary_party'] == "democratic":
-                can.drawString(112, 649, 'X') # Dem Primary Ballots
+                can.drawString(113, 567, 'X') # Dem Primary Ballots
             elif request.form['permanent_absentee_primary_party'] == "republican":
-                can.drawString(220, 649, 'X') # Rep Primary Ballots
+                can.drawString(221, 567, 'X') # Rep Primary Ballots
             elif request.form['permanent_absentee_primary_party'] == "no_primary":
-                can.drawString(317, 649, 'X') # No Primary Ballots
+                can.drawString(320, 567, 'X') # No Primary Ballots
         # else:
         #     # default to no primary
         #     can.drawString(317, 649, 'X') # No Primary Ballots
     else:
-        can.drawString(452, 681, 'X') # Vote by Mail in All Elections No
+        can.drawString(371, 600, 'X') # Vote by Mail in All Elections No
 
         if 'election_type' in request.form:
             if request.form['election_type'] == "general_special":
-                can.drawString(286, 634, 'X')  # Gen/Spec
+                can.drawString(285, 639, 'X')  # Gen/Spec
             elif request.form['election_type'] == "democratic":
-                can.drawString(405, 634, 'X') # Dem Prim
+                can.drawString(405, 639, 'X') # Dem Prim
             elif request.form['election_type'] == "republican":
-                can.drawString(504, 634, 'X') # Republican Primary
+                can.drawString(504, 639, 'X') # Republican Primary
 
         if 'election_date' in request.form and request.form['election_date'] != "":
             election_date = date.fromisoformat(request.form['election_date']) # YYYY-MM-DD
             # `election_date` should be in `config.UPCOMING_ELECTIONS`
-            can.drawString(196, 622, election_date.strftime('%m'))  # Month of Election
-            can.drawString(245, 622, election_date.strftime('%d'))  # Day of Election
-            can.drawString(289, 622, election_date.strftime('%Y'))  # Year of Election
+            can.drawString(196, 615, election_date.strftime('%m'))  # Month of Election
+            can.drawString(245, 615, election_date.strftime('%d'))  # Day of Election
+            can.drawString(289, 615, election_date.strftime('%Y'))  # Year of Election
 
-        can.drawString(422, 622, request.form['registered_county'])  # City/County
+        can.drawString(422, 615, request.form['registered_county'])  # City/County
 
-        can.drawString(153, 532, request.form['ballot_address'])
-        can.drawString(532, 532, request.form['ballot_apt'])
-        can.drawString(133, 514, request.form['ballot_city'])
-        can.drawString(320, 514, request.form['ballot_state'])
-        can.drawString(405, 514, '    '.join(request.form['ballot_zip']))
-        can.drawString(546, 514, request.form['ballot_country'])
+        can.drawString(153, 525, request.form['ballot_address'])
+        can.drawString(532, 525, request.form['ballot_apt'])
+        can.drawString(133, 508, request.form['ballot_city'])
+        can.drawString(320, 508, request.form['ballot_state'])
+        can.drawString(405, 508, '    '.join(request.form['ballot_zip']))
+        can.drawString(546, 508, request.form['ballot_country'])
 
-    can.drawString(150, 593, request.form['address'])
-    can.drawString(527, 593, request.form['apt'])
-    can.drawString(136, 575, request.form['city'])
-    can.drawString(393, 575, '     '.join(request.form['zip']))
+    can.drawString(150, 682, request.form['address'])
+    can.drawString(527, 682, request.form['apt'])
+    can.drawString(136, 663, request.form['city'])
+    can.drawString(393, 663, '     '.join(request.form['zip']))
 
     can.drawString(181, 432, request.form['former_name'])
     can.drawString(180, 416, request.form['former_address'])
+    can.drawString(145, 400, request.form['former_city'])
+    can.drawString(378, 400, request.form['former_state'])
+    can.drawString(468, 400, '     '.join(request.form['former_zip']))
     # can.drawString(158, 448, 'City')
     # can.drawString(398, 448, 'State')
     # can.drawString(477, 448, '     '.join('20171'))
@@ -102,35 +106,35 @@ def write_pdf(application_id, request, lang):
 
     can.drawString(128, 301, 'X' if request.form.get(
         'assistance_check') == 'true' else '',)
-    can.drawString(200, 257, request.form['assistant_name'])
-    # can.drawString(510, 258, 'Assistant Phone')
-    can.drawString(200, 239, request.form['assistant_address'])
-    can.drawString(560, 239, request.form['assistant_apt'])
-    can.drawString(145, 225, request.form['assistant_city'])
-    can.drawString(378, 225, request.form['assistant_state'])
-    can.drawString(468, 225, '      '.join(request.form['assistant_zip']))
-    can.drawString(210, 176, request.form['assistant_name'])
+    can.drawString(200, 249, request.form['assistant_name'])
+    can.drawString(510, 248, request.form['assistant_phone'])
+    can.drawString(200, 232, request.form['assistant_address'])
+    can.drawString(550, 237, request.form['assistant_apt'])
+    can.drawString(145, 218, request.form['assistant_city'])
+    can.drawString(378, 218, request.form['assistant_state'])
+    can.drawString(468, 218, '      '.join(request.form['assistant_zip']))
+    can.drawString(210, 169, request.form['assistant_name'])
     if request.form['assistant_name']:
-        can.drawString(450, 176, today_date.strftime('%m'))
-        can.drawString(490, 176, today_date.strftime('%d'))
-        can.drawString(540, 176, today_date.strftime('%y'))
+        can.drawString(450, 166, today_date.strftime('%m'))
+        can.drawString(490, 166, today_date.strftime('%d'))
+        can.drawString(540, 166, today_date.strftime('%y'))
 
-    can.drawString(175, 467, request.form['email'])
+    can.drawString(175, 464, request.form['email'])
     phonenumber = request.form['phonenumber']  # ddd-ddd-dddd
     # faster than regex:
     area_code = phonenumber[:3]
     central_office_code = phonenumber[4:7]
     line_number = phonenumber[8:]
-    can.drawString(169, 487, "      ".join(area_code))
-    can.drawString(255, 487, "      ".join(central_office_code))
-    can.drawString(337, 487, "      ".join(line_number))
+    can.drawString(169, 481, "      ".join(area_code))
+    can.drawString(255, 481, "      ".join(central_office_code))
+    can.drawString(337, 481, "      ".join(line_number))
 
-    can.drawString(260, 110, f'/s/ {request.form["signature"].strip().title()}')
+    can.drawString(260, 100, f'/s/ {request.form["signature"].strip().title()}')
     can.setFont('Helvetica', 10)
-    can.drawString(320, 135, 'This absentee ballot request contains an electronic signature.')
-    can.drawString(482, 110, today_date.strftime('%m'))
-    can.drawString(522, 110, today_date.strftime('%d'))
-    can.drawString(563, 110, today_date.strftime('%y'))
+    can.drawString(320, 128, 'This absentee ballot request contains an electronic signature.')
+    can.drawString(482, 100, today_date.strftime('%m'))
+    can.drawString(522, 100, today_date.strftime('%d'))
+    can.drawString(563, 100, today_date.strftime('%y'))
 
     can.save()
     packet.seek(0)
